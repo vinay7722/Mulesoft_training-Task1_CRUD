6ab9bb3d8e1641dff4aa0e66143ee77921c845b2
vinay7722 <vchauhan765@rku.ac.in>
2022-02-09 13:40:17 +0530

    delete functionality added

---
+++ fetch.py
@@ -0,0 +1,13 @@
+import sqlite3  #necessary packages imported
+
+connection = sqlite3.connect('movies.db')   # connect method is responsible for creating the database
+cursor = connection.cursor()
+
+#select query
+selectCommand = "SELECT * FROM details"
+
+# execute method method is responsible for firing the query that we pass as a parameter
+cursor.execute(selectCommand)
+
+movies = cursor.fetchall()
+print(movies)
