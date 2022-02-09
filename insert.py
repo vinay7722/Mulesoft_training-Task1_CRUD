4b8b2b296a0e2efeb78bcbfa3a488d9887aa7c5d
vinay7722 <vchauhan765@rku.ac.in>
2022-02-09 13:37:34 +0530

    insert functionality added

---
+++ insert.py
@@ -0,0 +1,11 @@
+import sqlite3  #necessary packages imported
+
+connection = sqlite3.connect('movies.db')   # connect method is responsible for creating the database
+cursor = connection.cursor()
+
+# insert query
+insertQuery = "INSERT INTO details VALUES ('The Avengers', 'Robert Downey Jr.', 'Scarlett Johansson', 'Joe Russo', 2012)"
+
+# execute method method is responsible for firing the query that we pass as a parameter
+cursor.execute(insertQuery)
+connection.commit()
